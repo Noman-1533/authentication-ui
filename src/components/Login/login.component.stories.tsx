4d0392303b2c03d84b5060a4c725c842736b1efa
Md. Riajul Alam Bhuiyan <riajul.alam@brainstation-23.com>
2024-11-12 14:00:46 +0600

    implement depends on functinality & add storybook

--- src/components/Login/login.component.stories.tsx
+++ src/components/Login/login.component.stories.tsx
@@ -330,3 +330,85 @@ SignUpFormWithTerms.args = {
   ],
 };
 
+
+export const SignUpFormWithDependsOn = Template.bind({});
+SignUpFormWithDependsOn.args = {
+  formName: "Sign Up",
+  button1Label: "Register",
+  button1Color: "bg-purple-500",
+  button1Size: "px-10 py-4",
+  button1Position: "right",
+  button1CheckValidation: true,
+  button2Label: "Cancel",
+  button2Color: "bg-gray-500",
+  button2Size: "px-4 py-2",
+  button2Position: "left",
+  button2CheckValidation: false,
+  rememberMe: false,
+  isReset: false,
+  formField: [
+    {
+      fieldLabel: "Username",
+      fieldType: "text",
+      fieldName: "username",
+      placeholder: "Enter your username",
+      validators: [
+        { type: "required", message: "Username is required" },
+        { type: "minLength", value: 3, message: "Minimum 3 characters" },
+      ],
+    },
+    {
+      fieldLabel: "Email",
+      fieldType: "email",
+      fieldName: "email",
+      placeholder: "Enter your email",
+      validators: [
+        { type: "required", message: "Email is required" },
+        { type: "email", message: "Invalid email format" },
+      ],
+    },
+    {
+      fieldLabel: "Password",
+      fieldType: "password",
+      fieldName: "password",
+      placeholder: "Enter your password",
+      validators: [
+        { type: "required", message: "Password is required" },
+        { type: "minLength", value: 8, message: "Minimum 8 characters" },
+      ],
+    },
+    {
+      fieldLabel: "Confirm Password",
+      fieldType: "password",
+      fieldName: "confirmPassword",
+      placeholder: "Confirm your password",
+      dependsOn:{
+        fieldName: "password",
+        message: "Passwords do not match",
+      }
+    },
+    {
+      fieldLabel: "Accept our terms and service",
+      fieldType: "checkbox",
+      fieldName: "terms",
+      validators: [{ type: "required", message: "Password is required" }],
+    },
+  ],
+  buttons: [
+    {
+      buttonLabel: "Sign In",
+      buttonColor: "bg-green-500",
+      buttonSize: "px-8 py-4",
+      buttonPosition: "left",
+      checkValidation: true,
+    },
+    {
+      buttonLabel: "Cancel",
+      buttonColor: "bg-gray-500",
+      buttonSize: "px-4 py-2",
+      buttonPosition: "right",
+      checkValidation: false,
+    },
+  ],
+};
+
